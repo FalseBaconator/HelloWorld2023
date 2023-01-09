9291f2bc393be88f324a6ea9ba2fe5909c0bca8d
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-09 13:58:50 -0400

    Added Instructions to close program

--- HelloWorld2023/Program.cs
+++ HelloWorld2023/Program.cs
@@ -11,6 +11,8 @@ namespace HelloWorld2023
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey(true);
         }
     }
